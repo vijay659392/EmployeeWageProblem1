cf4d50f84d46b992da01a4c9fc004764043d96ba
vijay <vijaydurkikar1@gmail.com>
2022-12-29 00:02:48 +0530

    Employee Wage

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -12,6 +12,10 @@ namespace EmployeeWageProblem
         public static void CheckAttendence()
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(2);
@@ -19,11 +23,16 @@ namespace EmployeeWageProblem
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
+
     }
 }
