c5f1834fc00a2aa0f14af67c57b9ff42f9a1741d
Durkikar Vijay <120495362+vijay659392@users.noreply.github.com>
2022-12-30 00:34:22 +0530

    Update EmployeeAttendence.cs

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,37 +6,38 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageProblem
 {
+   public class EmployeeAttendence
+   {
 
+      public static void CheckAttendance()
+      {
+         int IS_FULL_TIME = 1;
+         int IS_PART_TIME = 2;
+         int EMP_RATE_PER_HOUR = 20;
 
-    public static void CheckAttendance()
-    {
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
+         int empHrs = 0;
+         int empWage = 0;
 
-        int empHrs = 0;
-        int empWage = 0;
+         Random random = new Random();
+         int empCheck = random.Next(3);
 
-        Random random = new Random();
-        int empCheck = random.Next(3);
-
-        if(empCheck== IS_FULL_TIME)
-        {
-            Console.WriteLine("Employee is Present");
-            empHrs = 8;
-        }
-        else if(empCheck==IS_FULL_TIME)
-        {
-            Console.WriteLine("Employee is Part time");
-            empHrs = 4;
-        }
-        else
-        {
-            Console.WriteLine("Employee is Absent");
-            empHrs = 0; 
-        }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-
+         if(empCheck== IS_FULL_TIME)
+         {
+             Console.WriteLine("Employee is Present");
+             empHrs = 8;
+         }
+         else if(empCheck==IS_FULL_TIME)
+         {
+             Console.WriteLine("Employee is Part time");
+             empHrs = 4;
+         }
+         else
+         {
+             Console.WriteLine("Employee is Absent");
+             empHrs = 0; 
+         }
+         empWage = empHrs * EMP_RATE_PER_HOUR;
+       }     
         Console.WriteLine("Emp Wage: " + empWage);
     }
 }
